5fa61188fefce2c3effeaf18b799d20ea9d249b0
vandelay87 <akliaissat@outlook.com>
2026-03-06 16:04:34 +0000

    main element

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import './style.css'
 import { setupBoard } from './board.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <main>
     <h1>Sand box</h1>
     <canvas id="canvas" width="400" height="600">sand playground</canvas>
-  </div>
+  </main>
 `
 
 setupBoard(document.querySelector<HTMLCanvasElement>('#canvas')!)
